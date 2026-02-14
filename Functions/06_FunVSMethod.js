c4d2198057082551dbc2e89bad5945f430c43014
aadityap0ap <adityabharti450ap@gmail.com>
2026-02-14 13:43:25 +0530

    FunctionVSMethods

---
+++ Functions/06_FunVSMethod.js
@@ -0,0 +1,11 @@
+const maths = {
+    addition:function add(a,b){
+    return a+b;
+},
+    subtraction:function subtract(a,b){
+    return a - b;
+},
+    squaring:function square(a){
+    return a * a;
+}
+};
